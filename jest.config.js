db2b4758c9c1159507386fd02d9a247fafcc3aa3
Logan Davis <logankeahidavis@gmail.com>
2019-10-14 18:50:59 -0700

    Adds jest-coverage-config for Jenkins coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+const jestCoverageConfig = process.env.CI && require('@mixmaxhq/jest-coverage-config');
 module.exports = {
   testEnvironment: 'node',
   clearMocks: true,
+  ...jestCoverageConfig,
+  collectCoverageFrom: ['index.js'],
 };
